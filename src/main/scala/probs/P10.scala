54637a40aedc3819214e2b994f5500505cc70cfb
Martin Gladdish <martin@martingladdish.co.uk>
2014-08-12 17:38:41 +0100

    Problem 10 - neater solution just using map

--- src/main/scala/probs/P10.scala
+++ src/main/scala/probs/P10.scala
@@ -2,8 +2,7 @@ package probs
 
 object P10 {
 
-  def encode[T](l: List[T]): List[(Int, T)] = P09.pack(l).foldLeft(List[(Int, T)]())((result, item) =>
-    result :+ (item.length, item.head)
-  )
+  def encode[T](l: List[T]): List[(Int, T)] =
+    P09.pack(l).map(li => (li.length, li.head))
 
 }
