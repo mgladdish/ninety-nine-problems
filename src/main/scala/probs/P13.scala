751a96d072790060239e5d0a0ddbb5da0bbf8f42
Martin Gladdish <martin@martingladdish.co.uk>
2014-08-13 15:28:48 +0100

    Problem 13 - More readable, but a little verbose

--- src/main/scala/probs/P13.scala
+++ src/main/scala/probs/P13.scala
@@ -3,11 +3,15 @@ package probs
 object P13 {
 
   def encodeDirect[T](l: List[T]): List[(Int, T)] = l.foldRight(List[(Int, T)]())((item, result) =>
-    if (result.headOption.map(h => h._2 != item).getOrElse(true)) {
-      (1, item) +: result
-    } else {
-      result.headOption.map(h => (if (h._2 == item) h._1 + 1 else h._1, h._2)).getOrElse((1, item)) +: result.tail
-    }
+    result.headOption.map(h =>
+      if (h._2 == item) {
+        (h._1 + 1, item) +: result.tail
+      } else {
+        (1, item) +: result
+      }
+    ).getOrElse(
+        (1, item) +: result
+      )
   )
 
 }
