b25a20882b689a7c31dcd1083d5f3ba611e76b81
Martin Gladdish <martin@martingladdish.co.uk>
2014-08-14 09:41:15 +0100

    Problem 16 - neater functional solution after looking at solutions on line

--- src/main/scala/probs/P16.scala
+++ src/main/scala/probs/P16.scala
@@ -2,7 +2,9 @@ package probs
 
 object P16 {
 
-  def dropNth[T](n: Int, l: List[T]): List[T] = l.zipWithIndex.foldLeft(List[T]())(
-    (result, tuple) => if ((tuple._2 + 1) % n == 0) result else result :+ tuple._1
-  )
+  def dropNth[T](n: Int, l: List[T]): List[T] =
+    l.zipWithIndex
+      .filter(tuple => (tuple._2 + 1) % n  != 0)
+      .map(_._1)
+
 }
